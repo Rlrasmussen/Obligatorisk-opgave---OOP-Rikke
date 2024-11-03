da61ccce97139aa4d6b233eef49a6b57c4e8d826
Rlrasmussen <soede_rikke@hotmail.com>
2024-11-03 13:39:36 +0100

    Tilføjet Cage klasses og tilføjet metoden AddAnimal

---
+++ Obligatorisk opgave -  OOP Rikke/Cage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorisk_opgave____OOP_Rikke
+{
+    internal class Cage
+    {
+
+        #region field
+        private List<Animal> animals = new List<Animal>();
+
+        #endregion
+
+        #region property
+
+
+        #endregion
+
+        #region constructor
+
+        #endregion
+
+        #region method
+        public void AddAnimal(Animal animal)
+        {
+            foreach (var cagedAnimal in animals)
+            {
+                if (animals.Count == 0)
+                {
+                    animals.Add(animal);
+                    MainWindow.SetLabelOutput($"The {animal} is added to the cage");
+                    break;
+                }
+                if(cagedAnimal is Tiger && animal is Tiger)
+                {
+                    animals.Add(animal);
+                    MainWindow.SetLabelOutput($"The {animal} is added to the cage");
+                    break;
+                }
+                if (cagedAnimal is Tiger && animal is Parrot || cagedAnimal is Tiger && animal is Monkey)
+                {
+                    MainWindow.SetLabelOutput($"The {animal} can not be added to the tiger cage");
+                    break;
+                }
+                animals.Add(animal);
+                MainWindow.SetLabelOutput($"The {animal} is added to the cage");
+            }
+        }
+
+        #endregion
+    }
+}
